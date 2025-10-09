a533c4f9a24676bf6788cb4aca0f8a2fb1b54a2c
Matthew Dolman <matthew.dolman@krystal.io>
2025-10-09 09:32:40 +0100

    Pusing change to zipmap

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 data "katapult_disk_templates" "all" {}
 
 locals {
-  disk_templates = zipmap(data.katapult_disk_templates.all.templates[*].name, data.katapult_disk_templates.all.templates[*].id)
+  disk_templates = zipmap(data.katapult_disk_templates.all.templates[*].name, data.katapult_disk_templates.all.templates[*].permalink)
 
   ip_list = [ for each in katapult_ip.vm : each.value.id ]
 }
