4bcb8bc2e78a236dcaa111cd14ae3d99db8e7f4b
Matthew Dolman <matthew.dolman@krystal.io>
2025-10-09 09:59:03 +0100

    Updating locals

--- locals.tf
+++ locals.tf
@@ -3,5 +3,5 @@ data "katapult_disk_templates" "all" {}
 locals {
   disk_templates = zipmap(data.katapult_disk_templates.all.templates[*].name, data.katapult_disk_templates.all.templates[*].id)
 
-  ip_list = [ for each in katapult_ip.vm : each.value.id ]
+  ip_list = [ for each in katapult_ip.vm : each.id ]
 }
