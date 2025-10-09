d0a11d9e748738b5b57d98ace4ea9f1b8c9e8c42
jakekrystal <123396977+jakekrystal@users.noreply.github.com>
2025-10-09 17:18:04 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "vm_id" {
   value = katapult_virtual_machine.vm.id
 }
-output "sg_id" {
-  value = module.sg[0].sg_id
-}
 output "vm_ips" {
   value = katapult_ip.ip[*].address
+}
+output "sg_id" {
+  value = var.create_sg ? module.sg[0].sg_id : null
 }
